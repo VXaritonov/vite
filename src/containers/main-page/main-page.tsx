e958bd481838c6f9f8853ebc3985c5074f25a952
VXaritonov <vitalik_xaritonov_99@mail.ru>
2024-05-24 18:19:58 +0300

    feat(*): add react router dom

---
+++ src/containers/main-page/main-page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const MainPage = () => (
+    <div>
+        MainPage
+        <Outlet />
+    </div>
+);
+
+export default MainPage;
---
+++ src/routing.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import MainPage from "src/containers/main-page";
+import Page1 from "src/containers/page1";
+import Page2 from "src/containers/page2";
+import Page3 from "src/containers/page3";
+import ErrorPage from "src/containers/error-page";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainPage />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "page1",
+                element: <Page1 />,
+            },
+            {
+                path: "page2",
+                element: <Page2 />,
+            },
+            {
+                path: "page/:id",
+                element: <Page3 />,
+            },
+        ],
+    },
+]);
+
+const Routing = () => <RouterProvider router={router} />;
+
+export default Routing;
