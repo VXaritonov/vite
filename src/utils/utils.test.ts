6e8b25b022c647909728c6188164b184e53c6505
VXaritonov <vitalik_xaritonov_99@mail.ru>
2024-05-24 17:08:19 +0300

    feat(*): add jest test

---
+++ src/utils/utils.test.ts
@@ -0,0 +1,11 @@
+import * as utils from "./utils.tsx";
+
+describe("math module", () => {
+    test("adds 1 + 2 to equal 3", () => {
+        expect(utils.sum(1, 2)).toBe(3);
+    });
+
+    test("adds 1 * 2 to equal 2", () => {
+        expect(utils.mul(1, 2)).toBe(2);
+    });
+});
